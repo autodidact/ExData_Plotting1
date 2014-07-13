0239db354181890137e3571a76bb7e719bbaa1e5
Deepak Angrula <deepak.angrula@gmail.com>
2014-07-14 02:22:35 +0530

    improved plotting via direct to png plotting

--- assignment-solution/plot1.R
+++ assignment-solution/plot1.R
@@ -1,31 +1,37 @@
-readData <- function(filename) {
-  setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y"))
-  
-  colNames <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", 
-                 "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-  
-  
+readData <- function(filename) {  
   # First we will just read the first column from data and find our data boundaries
   # to use later when we read the full data.
-  classes <- c("myDate", rep("NULL", times=8)) # read only first column
+  classes <- c("character", rep("NULL", times=8)) # read only first column
   data <- read.table(filename, header=TRUE, sep=";", na.strings="?", colClasses=classes)
+  
+  ## transform first column to a date column for comparison
+  data$Date <- as.Date(data$Date, format="%d/%m/%Y") 
 
   min_row <- which.max(data$Date == as.Date("2007-02-01")) #first row to read
   max_row <- which.max(data$Date > as.Date("2007-02-02")) #first row to not read
   
   skip <- min_row ## number of rows to skip including header 
   nrows <- max_row - min_row 
+  colNames <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", 
+                "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
   
-  classes <- c("myDate","character",rep("numeric", times=7))
+  classes <- c("character","character",rep("numeric", times=7))
+  
+  ## read all columns but only the rows that we require as computed above.
   data <- read.table(filename, header=FALSE, sep=";", na.strings="?", colClasses=classes, col.names=colNames, skip=skip, nrows=nrows)
-  data$datetime = strptime(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
+  data$datetime = strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+  
   data
 }
 
 data <- readData("data//household_power_consumption.txt")
 
+# open png device
+png(file="assignment-solution/plot1.png")
+
+par(mfrow=c(1,1))
 with(data, hist(Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)"))
 
-dev.copy(png, file="assignment-solution/plot1.png")
+#dev.copy(png, file="assignment-solution/plot1.png")
 dev.off()
 
